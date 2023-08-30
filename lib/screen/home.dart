5b7cea20f018472e12634369291ec97187ddd001
SEC-MASUM <masum.jess42@gmail.com>
2023-08-30 12:23:18 +0600

    Basic Api Call in flutter

---
+++ lib/screen/home.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Rest API Call"),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          final name = user['name']['first'];
+          final email = user['email'];
+          final imageUrl = user['picture']['thumbnail'];
+          return ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(imageUrl),
+            ),
+            title: Text(name),
+            subtitle: Text(email),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+      ),
+    );
+  }
+
+  void fetchUsers() async {
+    print("FetchUsers Called");
+    const url = "https://randomuser.me/api/?results=100";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final jsonBody = jsonDecode(body);
+    setState(() {
+      users = jsonBody['results'];
+    });
+    print("fetchUsers completed");
+  }
+}
